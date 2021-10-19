8b33c62c971c5461c772a34abb9c263a92fdb2f6
remtav <34774759+remtav@users.noreply.github.com>
2021-10-18 23:00:15 -0400

    scan_from_inference.py: start post-processing from inference

--- scan_from_inference.py
+++ scan_from_inference.py
@@ -6,6 +6,7 @@
 # python scan.py --images sample_images
 
 # Scanned images will be output to directory named 'output'
+import glob
 import re
 from io import BytesIO
 from pathlib import Path
@@ -139,8 +140,9 @@ class DocScanner(object):
         # dilate helps to remove potential holes between edge segments
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(MORPH,MORPH))
         dilated = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
-        # cv2.imshow("Dilated", dilated)
-        # cv2.waitKey()
+        if debug:
+            cv2.imshow("Dilated", dilated)
+            cv2.waitKey()
 
         # find edges and mark them in the output map using the Canny algorithm
         # edged = cv2.Canny(dilated, 0, CANNY)
@@ -155,6 +157,8 @@ class DocScanner(object):
         cnts_grid = []
         areas = []
         peris = []
+        discarded_peris = []
+        discarded_approx = []
         for c in contours:
             area = cv2.contourArea(c)
             areas.append(area)
@@ -165,13 +169,21 @@ class DocScanner(object):
                 approx = cv2.approxPolyDP(c, 0.02 * peri, True)
                 # if the contour has four vertices, then we have found
                 # rectangular contours
-                if len(approx) == 4 and min_peri < peri < max_peri:
-                    cnts_grid.append(c)
-                    peris.append(peri)
+                if len(approx) == 4:
+                    if min_peri < peri < max_peri:
+                        cnts_grid.append(c)
+                        peris.append(peri)
+                    else:
+                        discarded_peris.append(peri)
+                else:
+                    discarded_approx.append(len(approx))
 
         return cnts_grid, areas, peris
 
-    def find_aoi(self, scanned, blur_kernel_size=5, debug=False):
+    def find_aoi(self, scanned, blur_kernel_size=5,
+                 min_area=1500, max_area=2300,
+                 min_peri=150, max_peri=210,
+                 debug=False):
         blur = cv2.GaussianBlur(scanned, (blur_kernel_size, blur_kernel_size), 0)
         if debug:
             cv2.imshow("Blur", blur)
@@ -182,65 +194,90 @@ class DocScanner(object):
             cv2.imshow("thresh2", thresh2)
             cv2.waitKey()
 
-        grid_contours, areas, peris = self.get_grid_contour(thresh2)
+        grid_contours, areas, peris = self.get_grid_contour(thresh2,
+                                                            min_area=min_area,
+                                                            max_area=max_area,
+                                                            min_peri=min_peri,
+                                                            max_peri=max_peri)
 
         if debug:
-            cv2.drawContours(scanned, grid_contours, -1, (0, 0, 255), 1)
-            cv2.imshow("Contours", scanned)
+            scanned_ctns = scanned.copy()
+            cv2.drawContours(scanned_ctns, grid_contours, -1, 122, 3)
+            cv2.imshow("Contours", scanned_ctns)
             cv2.waitKey()
-            cv2.destroyAllWindows()
 
         #CREATING AOI FROM GRID CONTOURS
         try:
             concat = np.concatenate(grid_contours)
             hull = cv2.convexHull(concat)
-            x, y, w, h = cv2.boundingRect(concat)
 
-            return (x, y, w, h), hull, grid_contours, areas, peris
+            return concat, hull, grid_contours, areas, peris
 
         except ValueError as e:
             print(e)
             return
+
+    def load_images(self, path):
+        p = str(Path(path))  # os-agnostic
+        p = os.path.abspath(p)  # absolute path
+        if '*' in p:
+            files = sorted(glob.glob(p, recursive=True))  # glob
+        elif os.path.isdir(p):
+            files = sorted(glob.glob(os.path.join(p, '*.*')))  # dir
+        elif os.path.isfile(p):
+            files = [p]  # files
+        else:
+            raise Exception('ERROR: %s does not exist' % p)
+
+        images = [x for x in files if Path(x).suffix.lower() == '.png']
+        return images
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("--images", help="Directory of images to be scanned")
     ap.add_argument("--preds", help="Path to directory containing text predictions")
-    ap.add_argument("-d", help='debug mode')
+    ap.add_argument("-d", action='store_true', help='debug mode')
     args = vars(ap.parse_args())
     im_dir = Path(args["images"]) if args["images"] else None
     if not im_dir.is_dir():
-        raise NotADirectoryError(f'Images directory not found: {args["images"]}')
+        warnings.warn(f'Images directory not found: {args["images"]}')
     pred_dir = Path(args["preds"]) if args["preds"] else None
     if not pred_dir.is_dir():
         raise NotADirectoryError(f'Predictions directory not found: {args["images"]}')
-    debug = True if args["d"] and args["d"] == 'True' else False
+    debug = args["d"]
 
     scanner = DocScanner()
 
     valid_formats = [".jpg", ".jpeg", ".jp2", ".png", ".bmp", ".tiff", ".tif"]
 
+    images = scanner.load_images(im_dir)
+
     # Scan all valid images in directory specified by command line argument --images <IMAGE_DIR>
-    pred_globbed = pred_dir.glob(f"*out.txt")
+    #pred_globbed = list(pred_dir.glob(f"**/*out.txt"))
 
     inf_pairs = []
-    for pred in tqdm(pred_globbed):
-        im_glob = list(im_dir.glob(f'**/{pred.stem}.png'))
-        if len(im_glob) > 1:
-            raise ValueError(f'There should only be one image for each inference. Got {len(im_glob)}\n'
-                             f'Prediction: {pred}\n'
-                             f'Images found: {im_glob}\n')
-        elif len(im_glob) == 0:
+    for img in tqdm(images):
+        img_district = [dir for dir in img.split('/') if 'District' in dir][0]
+        pred = list(pred_dir.glob(f"{img_district}/{Path(img).stem}.txt"))
+        # for pred_file in pred_globbed:
+        #     if pred_file.stem == Path(img).stem and pred_file.parts[-2] == img_district:
+        #         pred = pred_file
+        if len(pred) > 1:
+            raise ValueError(f'There should only be one prediction for each image. Got {len(pred)}\n'
+                             f'Image: {img}\n'
+                             f'Predictions found: {pred}\n')
+        elif len(pred) == 0:
             warnings.warn(f'No image found to match prediction:\n'
                           f'{pred}')
         else:
-            im = im_glob[0]
-            inf_pairs.append((im, pred))
+            pred = pred[0]
+            inf_pairs.append((img, pred))
 
     print(f'Found {len(inf_pairs)} pairs of images and predictions. Detecting numbers in predicted bboxes...')
 
     for img, prediction in inf_pairs:
+        img_district = [dir for dir in img.split('/') if 'District' in dir][0]
         inf_data = pandas.read_table(prediction, sep=' ', header=None)
         scanned_color = cv2.imread(str(img))
         scanned = cv2.cvtColor(scanned_color, cv2.COLOR_BGR2GRAY)
@@ -251,180 +288,81 @@ if __name__ == "__main__":
             scanned_color = cv2.resize(scanned_color, (round(scanned_color.shape[1] * ratio), 1024), interpolation=cv2.INTER_LINEAR)
             scanned = cv2.resize(scanned, (round(scanned.shape[1]*ratio), 1024), interpolation=cv2.INTER_NEAREST)
 
-        # find grid
-        bbox, hull, grid_contours, areas, peris = scanner.find_aoi(scanned)  # bbox: x, y, w, h
-
         # BLACK OUT CHECKED NUMBERS IN IMAGES
         scanned_draw = scanned.copy()
-        #mask = np.zeros((scanned.shape), np.uint8)
+
         conf_threshold = 0.77  # FIXME: softcode
+        pred_areas = []
+        pred_peris = []
+        pred_ratios = []
         for row in inf_data.itertuples():
             conf = row[-2]
             if conf > conf_threshold:
                 x_rel, y_rel, w_rel, h_rel = row[-6:-2]
-                pt1 = int((x_rel-w_rel/2)*scanned.shape[1]), int((y_rel-h_rel/2)*scanned.shape[0])
-                pt2 = int((x_rel+w_rel/2)*scanned.shape[1]), int((y_rel+h_rel/2)*scanned.shape[0])
-                cv2.rectangle(scanned_draw, pt1, pt2, 0, -1)
+                x = int(x_rel * scanned.shape[1])
+                y = int(y_rel * scanned.shape[0])
+                w = int(w_rel * scanned.shape[1])
+                h = int(h_rel * scanned.shape[0])
+                ratio = w / h
+                if 0.8 < ratio < 1.3:
+                    pt1 = int((x_rel-w_rel/2)*scanned.shape[1]), int((y_rel-h_rel/2)*scanned.shape[0])
+                    pt2 = int((x_rel+w_rel/2)*scanned.shape[1]), int((y_rel+h_rel/2)*scanned.shape[0])
+                    center = (x, y)
+                    #cv2.rectangle(scanned_draw, pt1, pt2, 0, -1)
+                    cv2.circle(scanned_draw, center, 1, 122, thickness=10)
+                    area = w*h
+                    peri = 2*w+2*h
+                    pred_areas.append(area)
+                    pred_peris.append(peri)
+                    pred_ratios.append(ratio)
 
         if debug:
-            cv2.imshow("Non-checked", scanned_draw)
+            cv2.imshow("Checked", scanned_draw)
             cv2.waitKey()
             cv2.destroyAllWindows()
 
-        continue
+        # find grid
+        mean_area = np.mean(pred_areas)
+        min_area = min(pred_areas)
+        max_area = max(pred_areas)
+        min_peri = min(pred_peris)
+        max_peri = max(pred_peris)
+        concat, hull, grid_contours, areas, peris = scanner.find_aoi(scanned,
+                                                                     min_area=min_area*0.65,
+                                                                     max_area=max_area*1.15,
+                                                                     min_peri=min_peri*0.75,
+                                                                     max_peri=max_peri*1.15,
+                                                                     debug=debug)  # bbox: x, y, w, h
+        bbox = cv2.boundingRect(concat)
 
-        # Output numbers of checked boxes
-
-        area_hull = cv2.contourArea(hull)
-        peri = cv2.arcLength(hull, True)
-
-        area_bb = ((bbox[0]+bbox[2])-bbox[0])*((bbox[1]+bbox[3])-bbox[1])
-        hull_bb_ratio = area_hull/area_bb*100
+        # This served to see how close the BBox is to the hull. Would be better IoU-based measure
+        # area_hull = cv2.contourArea(hull)
+        # peri = cv2.arcLength(hull, True)
+        #
+        # area_bb = ((bbox[0]+bbox[2])-bbox[0])*((bbox[1]+bbox[3])-bbox[1])
+        # hull_bb_ratio = area_hull/area_bb*100
 
         # DRAWING RECTANGLE ON AOI
         mask = np.zeros((scanned.shape), np.uint8)
-        # draw white rectangle on aoi
-        image = cv2.rectangle(mask, (x, y), (x + w, y + h), 255, -1)
-        # add black outline of thickness on non-aoi TODO: necessary to add this outline?
-        image = cv2.rectangle(mask, (x, y), (x + w, y + h), 0, 2)
-
-        out_ckd = np.zeros_like(scanned)
-        mask_ckd = mask.copy()
-        # BLACK OUT WHERE CONTOURS (non-checked grid items) ARE ON SCANNED IMAGE
-        for i, contour in enumerate(grid_contours):
-            # cont_hull = cv2.convexHull(contour)
-            xb, yb, wb, hb = cv2.boundingRect(contour)
-            # black out where contours are on scanned image
-            image = cv2.rectangle(mask_ckd, (xb, yb), (xb + wb, yb + hb), 0, -1)
-            # crop individual contour for saving
-            grid_item = scanned[yb:yb + hb, xb:xb + wb]
-
-        # where mask is white, restore original values (i.e. aoi values)
-        out_ckd[mask_ckd == 255] = scanned[mask_ckd == 255]  # only use if using previous for loop
-
-        # on out non checked, we don't restore original values, we only restore non checked grid items
-        out_non_ckd = np.zeros_like(scanned)
-        mask_non_ckd = mask.copy()
-
-        # IN AOI, KEEP EVERYTHING BLACK EXCEPT CONTOURS (non-checked grid items)
-        for i, contour in enumerate(grid_contours):
-            # cont_hull = cv2.convexHull(contour)
-            xb, yb, wb, hb = cv2.boundingRect(contour)
-            out_non_ckd[yb:yb + hb, xb:xb + wb] = scanned[yb:yb + hb, xb:xb + wb]
-
-        out_non_ckd_draw = out_non_ckd.copy()
-        # WRITING ONLY AOI AS IMAGE
-        outdir_trn = Path(f'/home/remi/Documents/sherbrooke_citoyen/training_img_15oct/{im.parent.parent.parent.name}')
-        Path.mkdir(outdir_trn, exist_ok=True)
-        outfile = outdir_trn / f'{im.stem}_trn.png'
-        outfile_draw = outdir_trn / f'{im.stem}_bbox.png'
-        # outfile = outdir_trn / f'{im.stem}_aoi.png'
-        # cv2.imwrite(str(outfile), out_ckd)
+        # draw white on aoi
+        aoi_mask = cv2.drawContours(mask, [hull], -1, 255, -1)
+        # where mask is black, paint original image in black
+        scanned_draw[mask == 0] = 0
 
         if debug:
-            cv2.imshow("New image", out_ckd)
+            cv2.imshow("AOI_Checked", scanned_draw)
             cv2.waitKey()
             cv2.destroyAllWindows()
 
-        # FROM HERE, WE FILTER GRID ITEMS THAT ARE CHECKED FOR SURE AND ADD THEM TO OUT_NON_CKD
-        # THIS WILL SERVE AS TRAINING DATA
-        blur2 = cv2.GaussianBlur(out_ckd, (5, 5), 0)
+        # WRITING ONLY AOI AS IMAGE
+        outdir_trn = Path(f'/home/remi/Documents/sherbrooke_citoyen/18oct_from_inf/{img_district}')
+        Path.mkdir(outdir_trn, exist_ok=True)
+        outfile = outdir_trn / f'{Path(img).stem}_aoi.png'
+        #outfile_draw = outdir_trn / f'{im.stem}_bbox.png'
+
         if debug:
-            cv2.imshow("Blur2", blur2)
+            cv2.imshow("aoi with checked", scanned_color)
             cv2.waitKey()
+            cv2.destroyAllWindows()
 
-        # DILATE WITH WHITE TO TRY AND GET INDIVIDUAL CHECKED GRID ITEMS
-        # dilate helps to separate checked "x" from sides of grid item
-        MORPH = 5
-        kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (MORPH, MORPH))
-        dilated2 = cv2.morphologyEx(blur2, cv2.MORPH_CLOSE, kernel2)
-        if debug:
-            cv2.imshow("Dilated2", dilated2)
-            cv2.waitKey()
-
-        # FIRST PASS TO GET INDIVIDUAL CHECKED GRID ITEMS
-        grid_contours2, areas, peris = scanner.get_grid_contour(dilated2)
-
-        trn_bbox = []
-        # ADD ITEMS WE KNOW FOR SURE ARE CHECKED (FILTER BY AREA, THEN SHAPE, THEN MEAN VALUE OR
-        # LARGEST CONTOUR IN SINGLE GRID ITEM --> SEE FILTER_CHECKED() FUNCTION FOR DETAILS)
-        for i, contour in enumerate(grid_contours2):
-            area = cv2.contourArea(contour)
-            areas.append(area)
-            # approx area of a single grid item
-            if 1500 < area < 2500:
-                ctn = DocScanner.filter_by_shape(contour)
-                if ctn is not None:
-                    xc, yc, wc, hc = cv2.boundingRect(contour)
-                    grid_item = scanned[yc:yc + hc, xc:xc + wc]
-                    grid_item = DocScanner.filter_checked(grid_item)
-                    if grid_item is not None:
-                        out_non_ckd[yc:yc + hc, xc:xc + wc] = scanned[yc:yc + hc, xc:xc + wc]
-                        out_non_ckd_draw[yc:yc + hc, xc:xc + wc] = scanned[yc:yc + hc, xc:xc + wc]
-                        cv2.rectangle(out_non_ckd_draw, (xc, yc), (xc + wc, yc + hc), 122, 2)
-                        trn_bbox.append((xc, yc, wc, hc))
-
-        # SECOND PASS TO GET CHECKED GRID ITEMS THAT ARE STUCK TOGETHER IN PAIRS (NO MORE THAN PAIR)
-        # MOST HORIZONTAL AND VERTICAL PAIRS ARE EXTRACTED AND KEPT FOR TRAINING
-        grid_contours3, areas, peris = scanner.get_grid_contour(dilated2, min_area=3000, max_area=5000,
-                                                                min_peri=200, max_peri=350)
-        print(im.stem)
-        print(len(grid_contours3))
-        for i, contour in enumerate(grid_contours3):
-            area = cv2.contourArea(contour)
-            areas.append(area)
-            xc, yc, wc, hc = cv2.boundingRect(contour)
-            grid_item_uncut = scanned[yc:yc + hc, xc:xc + wc]
-            if debug:
-                cv2.imshow(f"{im.stem}_{area}", grid_item_uncut)
-                cv2.waitKey()
-            if 3000 < area < 5000:
-                ctn = DocScanner.filter_by_shape(contour, min_peri=210, max_peri=350)
-                if ctn is None:
-                    continue
-                # FILLING OUTSIDE OF CONTOUR WITH BLACK
-                mask2 = np.zeros((scanned.shape), np.uint8)
-                cv2.drawContours(mask2, [contour], 0, 255, -1)
-                out2 = np.zeros_like(scanned)
-                out2[mask2 == 255] = scanned[mask2 == 255]
-                if debug:
-                    cv2.imshow(f"out2_{area}", out2)
-                    cv2.waitKey()
-                # crop individual contour for saving
-                if wc > hc:
-                    for x in range(xc, xc+wc, int(wc/2)):
-                        # crop individual grid item
-                        grid_item = out2[yc:yc + hc, x:x + int(wc/2)]
-                        if debug:
-                            cv2.imshow(f"grid item cut", grid_item)
-                            cv2.waitKey()
-                        grid_item = DocScanner.filter_checked(grid_item)
-                        if grid_item is not None:
-                            out_non_ckd[yc:yc + hc, x:x + int(wc/2)] = scanned[yc:yc + hc, x:x + int(wc/2)]
-                            bbox = (x, yc, int(wc/2), hc)
-                            out_non_ckd_draw[yc:yc + hc, x:x + int(wc/2)] = scanned[yc:yc + hc, x:x + int(wc/2)]
-                            cv2.rectangle(out_non_ckd_draw, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), 122, 2)
-                            trn_bbox.append(bbox)
-                else:
-                    for y in range(yc, yc+hc, int(hc/2)):
-                        grid_item = out2[y:y + int(hc/2), xc:xc + wc]
-                        if debug:
-                            cv2.imshow(f"grid item cut", grid_item)
-                            cv2.waitKey()
-                        grid_item = DocScanner.filter_checked(grid_item)
-                        if grid_item is not None:
-                            out_non_ckd[y:y + int(hc/2), xc:xc + wc] = scanned[y:y + int(hc/2), xc:xc + wc]
-                            bbox = (xc, y, wc, int(hc/2))
-                            out_non_ckd_draw[y:y + int(hc / 2), xc:xc + wc] = scanned[y:y + int(hc / 2), xc:xc + wc]
-                            cv2.rectangle(out_non_ckd_draw, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), 122, 2)
-                            trn_bbox.append(bbox)
-
-
-        with open(outdir_trn.parent / 'bingo_trn.csv', 'a') as fh:
-            line = f'{outfile};{trn_bbox}\n'
-            fh.write(line)
-
-        # Now that filtering of checked items is complete, restore original value where mask is black (non-aoi)
-        out_non_ckd[mask == 0] = scanned[mask == 0]
-        cv2.imwrite(str(outfile), out_non_ckd)
-        cv2.imwrite(str(outfile_draw), out_non_ckd_draw)
+        #cv2.imwrite(str(outfile), scanned_color)
